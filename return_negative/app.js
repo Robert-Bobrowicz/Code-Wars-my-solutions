5faa8bc36d3995cad03e63ed49956f8f69bede88
unknown <r.bobrowicz@live.com>
2022-02-12 20:28:33 +0100

    code wars: my solutions

---
+++ return_negative/app.js
@@ -0,0 +1,10 @@
+//return negative of a given value
+
+function returnNegative (n) {
+    if(n > 0) return -n;
+    return n;
+}
+
+console.log(returnNegative(0));
+console.log(returnNegative(-1));
+console.log(returnNegative(1));
