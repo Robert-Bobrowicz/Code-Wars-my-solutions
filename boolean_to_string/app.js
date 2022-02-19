c151ed7f7b51520e00b756670bafd9b60bf57728
unknown <r.bobrowicz@live.com>
2022-02-19 09:31:45 +0100

    code wars

---
+++ boolean_to_string/app.js
@@ -0,0 +1,11 @@
+
+function booleanToString(b){
+    // if (b === true) {
+    //     return "true";
+    // }
+    // return "false";
+    return String(b);
+}
+
+console.log(booleanToString(true));
+console.log(booleanToString(false));
